0e6f11a1606aaab3a712a0d673d6cf1c97aa6e3f
Kévin MAZE <kekeprojectphotmail.fr>
2024-08-16 17:01:53 +0200

    parcours css

--- src/components/Parcours/Parcours.jsx
+++ src/components/Parcours/Parcours.jsx
@@ -1,36 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './parcours.css'
 
 const Parcours = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
     return (
         <section className="parcours section">
             <h2 className="section__title">Parcours</h2>
-            <span className="section__subtitle">Mon parcours personnel</span>
+            <span className="section__subtitle">Mon parcours professionnel</span>
 
             <div className="parcours__container container">
                 <div className="parcours__tabs">
 
-                    <div className="parcours__button parcours__active button--flex">
+                    <div className={toggleState === 1 ? "parcours__button parcours__active button--flex" : "parcours__button button--flex"} onClick={() => toggleTab(1)}>
                         <i className="uil uil-graduation-cap parcours__icon"></i>Scolaire
                     </div>
 
-                    <div className="parcours__button button--flex">
+                    <div className={toggleState === 2 ? "parcours__button parcours__active button--flex" : "parcours__button button--flex"} onClick={() => toggleTab(2)}>
                         <i className="uil uil-briefcase-alt parcours__icon"></i>Experience
                     </div>
                 </div>
 
                 <div className="parcours__section">
-                    <div className="parcours__content parcours__content-active">
+
+                    <div className={toggleState === 1 ? "parcours__content parcours__content-active" : "parcours__content"}>
                         <div className="parcours__data">
-                            <div></div>
-                            <div>
-                                <span className="parcours__rounder"></span>
-                                <span className="parcours__line"></span>
-                            </div>
                             <div>
                                 <h3 className="parcours__title">Formation Dev Web et Web mobile</h3>
                                 <span className="parcours__subtitle">Studi Ecole en ligne</span>
                                 <div className="parcours__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2023 - Mars 2024
+                                </div>
+                            </div>
+                            <div></div>
+                            <div>
+                                <span className="parcours__rounder"></span>
+                                <span className="parcours__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="parcours__data">
+                            <div>
+                                <h3 className="parcours__title">Formation Dev web et web mobile</h3>
+                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
+                                <div className="parcours__calender">
                                     <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
+                                </div>
+                            </div>
+                            <div></div>
+                            <div>
+                                <span className="parcours__rounder"></span>
+                                <span className="parcours__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="parcours__data">
+                            <div>
+                                <h3 className="parcours__title">Autre chose test</h3>
+                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
+                                <div className="parcours__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2023 - Mars 2024
+                                </div>
+                            </div>
+                            <div></div>
+                            <div>
+                                <span className="parcours__rounder"></span>
+                                <span className="parcours__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="parcours__data">
+                            <div>
+                                <h3 className="parcours__title">Oula ça fait beaucoup</h3>
+                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
+                                <div className="parcours__calender">
+                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
+                                </div>
+                            </div>
+                            <div></div>
+                            <div>
+                                <span className="parcours__rounder"></span>
+                                <span className="parcours__line"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={toggleState === 2 ? "parcours__content parcours__content-active" : "parcours__content"}>
+                        <div className="parcours__data">
+                            <div></div>
+                            <div>
+                                <span className="parcours__rounder"></span>
+                                <span className="parcours__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="parcours__title">Bidule truc machin</h3>
+                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
+                                <div className="parcours__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2023 - Mars 2024
                                 </div>
                             </div>
                         </div>
@@ -42,7 +111,7 @@ const Parcours = () => {
                                 <span className="parcours__line"></span>
                             </div>
                             <div>
-                                <h3 className="parcours__title"></h3>
+                                <h3 className="parcours__title">Wouha</h3>
                                 <span className="parcours__subtitle">Studi Ecole en ligne</span>
                                 <div className="parcours__calender">
                                     <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
@@ -57,10 +126,10 @@ const Parcours = () => {
                                 <span className="parcours__line"></span>
                             </div>
                             <div>
-                                <h3 className="parcours__title"></h3>
+                                <h3 className="parcours__title">Autre chose test</h3>
                                 <span className="parcours__subtitle">Studi Ecole en ligne</span>
                                 <div className="parcours__calender">
-                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
+                                    <i className="uil uil-calendar-alt"></i> 2023 - Mars 2024
                                 </div>
                             </div>
                         </div>
@@ -72,7 +141,7 @@ const Parcours = () => {
                                 <span className="parcours__line"></span>
                             </div>
                             <div>
-                                <h3 className="parcours__title"></h3>
+                                <h3 className="parcours__title">Oula ça fait beaucoup</h3>
                                 <span className="parcours__subtitle">Studi Ecole en ligne</span>
                                 <div className="parcours__calender">
                                     <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
@@ -81,67 +150,6 @@ const Parcours = () => {
                         </div>
                     </div>
 
-                    <div className="parcours__content">
-                        <div className="parcours__data">
-                            <div></div>
-                            <div>
-                                <span className="parcours__rounder"></span>
-                                <span className="parcours__line"></span>
-                            </div>
-                            <div>
-                                <h3 className="parcours__title">Formation Dev Web et Web mobile</h3>
-                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
-                                <div className="parcours__calender">
-                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="parcours__data">
-                            <div></div>
-                            <div>
-                                <span className="parcours__rounder"></span>
-                                <span className="parcours__line"></span>
-                            </div>
-                            <div>
-                                <h3 className="parcours__title"></h3>
-                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
-                                <div className="parcours__calender">
-                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="parcours__data">
-                            <div></div>
-                            <div>
-                                <span className="parcours__rounder"></span>
-                                <span className="parcours__line"></span>
-                            </div>
-                            <div>
-                                <h3 className="parcours__title"></h3>
-                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
-                                <div className="parcours__calender">
-                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="parcours__data">
-                            <div></div>
-                            <div>
-                                <span className="parcours__rounder"></span>
-                                <span className="parcours__line"></span>
-                            </div>
-                            <div>
-                                <h3 className="parcours__title"></h3>
-                                <span className="parcours__subtitle">Studi Ecole en ligne</span>
-                                <div className="parcours__calender">
-                                    <i className="uil uilcalendar-alt"></i> 2023 - Mars 2024
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 
             </div>
