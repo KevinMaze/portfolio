bdf2736753ce26e513b4d19985333ef481b28b64
MAZE Kevin <kekeproject@hotmail.fr>
2026-04-28 17:11:11 +0200

    change browers

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
-import Home from './components/Home/Home';
-import Services from './components/Services/Services';
-import About from './components/About/About';
-import Header from './components/Header/Header';
+import Portfolio from '../components/Portfolio/Portfolio';
+import Contact from '../components/Contact/Contact';
+import Home from '../components/Home/Home';
+import Services from '../components/Services/Services';
+import About from '../components/About/About';
+import Header from '../components/Header/Header';
 
 function App() {
   return (
